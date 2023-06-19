99c36c6aa1f7fcc185faf4c1d3b2acbfd2a76dec
brunolopez5 <chubybuddy5@gmail.com>
2023-06-19 14:48:51 +0000

    is positive, equal, or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+/** main - printing if n is positive, equal, or negative 
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n>0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n==0)
+	{
+		printf("%d is equal\n", n);
+	}
+	else (n<0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return (0);
+}
+
