ecd0f993d5fe6a2b92cd2078d7e73afd78dc2375
brunolopez5 <chubybuddy5@gmail.com>
2023-06-20 15:23:47 +0000

    print # 0>10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *main - Print all single digit # of base 10 starting from 0 .
+ *
+ *Return: 0 if success
+ *
+ */
+int main(void)
+{
+	int x;
+	for (x = 48; x < 58; x++)
+	{
+		putchar (x)
+		if (x < 57)
+		{
+			putchar (44)
+			putchar (32)
+		}
+	}
+putchar ('\n');
+return (0);
+}
