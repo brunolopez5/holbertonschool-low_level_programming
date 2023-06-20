6d21d5cc46764e42a0f63c6090a3a653d47dac18
brunolopez5 <chubybuddy5@gmail.com>
2023-06-20 13:23:30 +0000

    alphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -4,9 +4,9 @@
  *Return: 0 if success
  *
  */
-int main(void)
+int main (void)
 {
-	char alp[26] = {'a','b',c'd','e','f','g','h','i','j','k',
+	char alp[26] = {'a','b','c','d','e','f','g','h','i','j','k',
 	'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
 
 	int i;
