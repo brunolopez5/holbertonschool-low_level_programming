005aed893f8367d948753366a5ef685ebc8b4e68
brunolopez5 <chubybuddy5@gmail.com>
2023-06-20 13:19:53 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Always 0 
+ *Return: 0 if success
+ *
+ */
+int main(void)
+{
+	char alp[26] = {'a','b',c'd','e','f','g','h','i','j','k',
+	'l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'}
+
+	int i;
+
+	for {i = 0; i < 26; i++}
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n')
+
+	return (0)
+}
