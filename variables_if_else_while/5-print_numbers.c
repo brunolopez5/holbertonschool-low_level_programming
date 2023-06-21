af55306e8091e0d10bfae270714448a7f8195c72
brunolopez5 <chubybuddy5@gmail.com>
2023-06-21 15:47:27 +0000

    try18

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -5,19 +5,14 @@
  *main - Print all single digit # of base 10 starting from 0 .
  *
  *Return: 0 if success
- *
  */
 int main(void)
 {
 	int x;
-	for (x = 48; x < 58; x++)
+
+	for (x = 48, x < 58; x++)
 	{
 		putchar(x);
-		if (x < 57)
-		{
-			putchar (44);
-			putchar (32);
-		}
 	}
 	putchar ('\n');
 	return (0);
